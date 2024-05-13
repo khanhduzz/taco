fc8d088528b8de50c84d4b9902d33ba648a2b2ab
Khanh Du <>
2024-05-13 16:17:26 +0700

    OAuth after remove secret keys, second time

---
+++ src/main/java/choco/taco/taco/configs/OAuth2LoginConfig.java
@@ -0,0 +1,14 @@
+package choco.taco.taco.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+public class OAuth2LoginConfig {
+
+}
--- src/main/java/choco/taco/taco/configs/SecurityConfig.java
+++ src/main/java/choco/taco/taco/configs/SecurityConfig.java
@@ -6,12 +6,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.AuthenticatedPrincipalOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.User;
@@ -39,29 +47,77 @@ public class SecurityConfig{
 //        };
 //    }
 
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((authorizeHttpRequests) ->
+//                authorizeHttpRequests.requestMatchers("/design", "/orders").hasRole("USER")
+//                .requestMatchers("/", "/**").permitAll()
+//                .anyRequest().authenticated()
+//        ).formLogin((e) -> e.loginPage("/login")
+//                .defaultSuccessUrl("/design")
+//        ).oauth2Login((e) -> e.loginPage("/login"));
+//        return http.build();
+//    }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((authorizeHttpRequests) ->
+//                authorizeHttpRequests.requestMatchers("/design", "/orders").hasRole("USER")
+//                        .requestMatchers("/", "/**").permitAll()
+//                        .anyRequest().authenticated()
+//        ).formLogin((e) -> e.loginPage("/login")
+//                .defaultSuccessUrl("/design")
+//        );
+//        return http.build();
+//    }
+//
+    /* CREATE FAKE USE WITH IN MEMORY USER DETAIL .... */
+//    @Bean
+//    public UserDetailsService userDetailService (PasswordEncoder encoder) {
+//        List<UserDetails> userLists = new ArrayList<>();
+//        userLists.add(
+//                new User("buzz", encoder.encode("password"),
+//                        List.of(new SimpleGrantedAuthority("ROLE_USER")))
+//        );
+//        userLists.add(
+//                new User("woddy", encoder.encode("password"),
+//                        List.of(new SimpleGrantedAuthority("ROLE_USER")))
+//        );
+//        return new InMemoryUserDetailsManager(userLists);
+//    }
+
+    /* FOR GOOGLE OAUTH this part is good, and run successfully.... */
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorizeHttpRequests) ->
-                authorizeHttpRequests.requestMatchers("/design", "/orders").hasRole("USER")
-                .requestMatchers("/", "/**").permitAll()
-                .anyRequest().authenticated()
-        ).formLogin((e) -> e.loginPage("/login")
-                .defaultSuccessUrl("/design")
-        );
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests((e) ->
+                e.anyRequest().authenticated()
+                ).oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailService (PasswordEncoder encoder) {
-        List<UserDetails> userLists = new ArrayList<>();
-        userLists.add(
-                new User("buzz", encoder.encode("password"),
-                        List.of(new SimpleGrantedAuthority("ROLE_USER")))
-        );
-        userLists.add(
-                new User("woddy", encoder.encode("password"),
-                        List.of(new SimpleGrantedAuthority("ROLE_USER")))
-        );
-        return new InMemoryUserDetailsManager(userLists);
-    }
+    /* OMIT THIS PART ------------------------------------------------------*/
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository() {
+//        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+//    }
+//
+//    @Bean
+//    public OAuth2AuthorizedClientService authorizedClientService(
+//            ClientRegistrationRepository clientRegistrationRepository) {
+//        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
+//    }
+//
+//    @Bean
+//    public OAuth2AuthorizedClientRepository authorizedClientRepository(
+//            OAuth2AuthorizedClientService authorizedClientService) {
+//        return new AuthenticatedPrincipalOAuth2AuthorizedClientRepository(authorizedClientService);
+//    }
+//
+//    private ClientRegistration googleClientRegistration() {
+//        return CommonOAuth2Provider.GOOGLE.getBuilder("google")
+//                .clientId("google-client-id")
+//                .clientSecret("google-client-secret")
+//                .build();
+//    }
+    /* OMIT THIS PART ------------------------------------------------------*/
 }
